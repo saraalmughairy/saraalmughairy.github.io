16bcd92195fea94bf57c2e156c9ca939332ee85e
sawraw <sawra@uw.edu>
2017-07-04 10:23:27 -0700

    added p5 sine wave to home page

---
+++ js/sketch.js
@@ -0,0 +1,91 @@
+// var yoff = 0.0;        // 2nd dimension of perlin noise
+
+// function setup() {
+//   canvas = createCanvas(windowWidth, 500);
+//   canvas.parent("p5");
+// }
+
+// function draw() {
+//   background(255);
+//   noStroke();
+
+//   fill(230,91,108);
+//   // We are going to draw a polygon out of the wave points
+//   beginShape(); 
+  
+//   var xoff = 0;       // Option #1: 2D Noise
+//   // var xoff = yoff; // Option #2: 1D Noise
+  
+//   // Iterate over horizontal pixels
+//   for (var x = 0; x <= width; x += 10) {
+//     // Calculate a y value according to noise, map to 
+    
+//     // Option #1: 2D Noise
+//     var y = map(noise(xoff, yoff), 0, 1, 200,300);
+
+//     // Option #2: 1D Noise
+//     // var y = map(noise(xoff), 0, 1, 200,300);
+    
+//     // Set the vertex
+//     vertex(x, y); 
+//     // Increment x dimension for noise
+//     xoff += 0.03;
+//   }
+//   // increment y dimension for noise
+//   yoff += 0.01;
+//   vertex(width, height);
+//   vertex(0, height);
+//   endShape(CLOSE);
+// }
+
+
+
+
+
+
+
+
+var xspacing = 16;    // Distance between each horizontal location
+var w;                // Width of entire wave
+var theta = 0.0;      // Start angle at 0
+var amplitude = 75.0; // Height of wave
+var period = 500.0;   // How many pixels before the wave repeats
+var dx;               // Value for incrementing x
+var yvalues;  // Using an array to store height values for the wave
+
+function setup() {
+  var canvas = createCanvas(windowWidth, 400);
+    canvas.parent('p5');
+
+  w = width+16;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w/xspacing));
+}
+
+function draw() {
+  background(255);
+  calcWave();
+  renderWave();
+}
+
+function calcWave() {
+  // Increment theta (try different values for 
+  // 'angular velocity' here)
+  theta += 0.02;
+
+  // For every x value, calculate a y value with sine function
+  var x = theta;
+  for (var i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x)*amplitude;
+    x+=dx;
+  }
+}
+
+function renderWave() {
+  noStroke();
+  fill(230,91,108);
+  // A simple way to draw the wave with an ellipse at each location
+  for (var x = 0; x < yvalues.length; x++) {
+    ellipse(x*xspacing, height/2+yvalues[x], 16, 16);
+  }
+}
